d1fea9c1d8ae429ef7432fa3e87eb976006182cd
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-04-17 16:50:14 -0300

    diminuindo tamanho profile e adicionando o profile info

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainProfileComponent } from './components/main-profile/main-profile.component';
+import { InfoTabComponent } from './components/info-tab/info-tab.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainProfileComponent
+    MainProfileComponent,
+    InfoTabComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolio/src/app/components/info-tab/info-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-tab',
+  templateUrl: './info-tab.component.html',
+  styleUrl: './info-tab.component.scss'
+})
+export class InfoTabComponent {
+
+}
