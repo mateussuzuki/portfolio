7d53cadd3fb96b4b2548a7b24c60995e22185ae8
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-04-18 21:11:53 -0300

    adicionando typescript e mudando padding

--- portfolio/src/app/modal/tecnos.modal.ts
+++ portfolio/src/app/modal/tecnos.modal.ts
@@ -3,10 +3,10 @@ import { Tecno } from "../interface/tecno.interface";
 export class Tecnos {
   tecnos:Tecno[] = [
     {img: "../../../assets/svgs/angular.svg", title: "Angular"},
-    {img: "../../../assets/svgs/react.svg", title: "React"},
     {img: "../../../assets/svgs/js.svg", title: "JavaScript"},
+    {img: "../../../assets/svgs/typeScript.svg", title: "TypeScript"},
     {img: "../../../assets/svgs/bootstrap.svg", title: "Bootstrap"},
     {img: "../../../assets/svgs/git.svg", title: "Git"},
-    {img: "../../../assets/svgs/mysql.svg", title: "Angular"},
+    {img: "../../../assets/svgs/mysql.svg", title: "MySql"},
   ]
 }
