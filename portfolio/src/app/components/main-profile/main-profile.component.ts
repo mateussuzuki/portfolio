d569fcbb7be091c3e167554dc645caccd2873dd2
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-04-24 15:52:48 -0300

    animação de copy ao clicar em algum contato

--- portfolio/src/app/components/main-profile/main-profile.component.ts
+++ portfolio/src/app/components/main-profile/main-profile.component.ts
@@ -6,10 +6,55 @@ import { Component } from '@angular/core';
   styleUrl: './main-profile.component.scss'
 })
 export class MainProfileComponent {
+  // copyText(id: string) {
+  //   const text = document.getElementById(id)?.textContent;
+  //   if (text) {
+  //     navigator.clipboard.writeText(text)
+  //   }
+  // }
+
+
+  // mostrarMensagemCopy(event: MouseEvent) {
+  //   const button = event.currentTarget as HTMLElement;
+  //   const mensagemCopy = button.querySelector('.mensagem-copy') as HTMLElement;
+  //   if (mensagemCopy) {
+  //     mensagemCopy.style.display = 'block';
+  //   }
+  // }
+
+  // ocultarMensagemCopy() {
+  //   const mensagensCopy = document.querySelectorAll('.mensagem-copy');
+  //   mensagensCopy.forEach(mensagem => {
+  //     const mensagemElement = mensagem as HTMLElement;
+  //     mensagemElement.style.display = 'none';
+  //   });
+  // }
+
   copyText(id: string) {
     const text = document.getElementById(id)?.textContent;
     if (text) {
       navigator.clipboard.writeText(text)
+        .then(() => {
+          this.mostrarMensagemCopy(id);
+          setTimeout(() => this.ocultarMensagemCopy(id), 1000);
+        })
     }
   }
+  
+  mostrarMensagemCopy(id: string) {
+    const mensagemCopy = document.getElementById(`${id}-copy`) as HTMLElement;
+    if (mensagemCopy) {
+      mensagemCopy.style.display = 'block';
+    }
+  }
+  
+  ocultarMensagemCopy(id: string) {
+    const mensagemCopy = document.getElementById(`${id}-copy`) as HTMLElement;
+    if (mensagemCopy) {
+      mensagemCopy.style.display = 'none';
+    }
+  }
+ 
 }
+
+
