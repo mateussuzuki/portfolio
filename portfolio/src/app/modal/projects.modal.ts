44355a8ce4897c6901c96ecb77bd105b83fd8e19
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-04-26 15:18:41 -0300

    alterando o href das imagens e svgs para deploy

--- portfolio/src/app/modal/projects.modal.ts
+++ portfolio/src/app/modal/projects.modal.ts
@@ -2,11 +2,11 @@ import { Project } from "../interface/project.interface";
 
 export class Projects {
   projects:Project[] = [
-      {img: "../../../assets/image/projetoValorant.jpg", 
+      {img: "./assets/image/projetoValorant.jpg", 
       title: "Projeto Valorant", 
       description:"Uma wiki do jogo Valorant consumindo uma API externa da própria empresa, nele utilizei o Angular e Boostrap", 
       class:"backPurple"},
-      {img: "../../../assets/image/projetoCrud.jpg", 
+      {img: "./assets/image/projetoCrud.jpg", 
       title: "Crud", 
       description:"Um simples crud com paginação e ordenação feita com Angular e MySql", 
       class:"backBlue"},
--- portfolio/src/app/modal/tecnos.modal.ts
+++ portfolio/src/app/modal/tecnos.modal.ts
@@ -2,11 +2,11 @@ import { Tecno } from "../interface/tecno.interface";
 
 export class Tecnos {
   tecnos:Tecno[] = [
-    {img: "../../../assets/svgs/angular.svg", title: "Angular", class:"backPurple"},
-    {img: "../../../assets/svgs/js.svg", title: "JavaScript", class:"backBlue"},
-    {img: "../../../assets/svgs/typeScript.svg", title: "TypeScript", class:"backBlue"},
-    {img: "../../../assets/svgs/bootstrap.svg", title: "Bootstrap", class:"backPurple"},
-    {img: "../../../assets/svgs/gitProject.svg", title: "Git", class:"backPurple"},
-    {img: "../../../assets/svgs/mysql.svg", title: "MySql", class:"backBlue"},
+    {img: "./assets/svgs/angular.svg", title: "Angular", class:"backPurple"},
+    {img: "./assets/svgs/js.svg", title: "JavaScript", class:"backBlue"},
+    {img: "./assets/svgs/typeScript.svg", title: "TypeScript", class:"backBlue"},
+    {img: "./assets/svgs/bootstrap.svg", title: "Bootstrap", class:"backPurple"},
+    {img: "./assets/svgs/gitProject.svg", title: "Git", class:"backPurple"},
+    {img: "./assets/svgs/mysql.svg", title: "MySql", class:"backBlue"},
   ]
 }
